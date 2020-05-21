28a5dfa64f0a9b337f85b954e4cfed268dd89bc7
Frank Morgner <frankmorgner@gmail.com>
2020-05-21 10:25:43 +0200

    a

--- .ci/ACardEmulator_build.bat
+++ .ci/ACardEmulator_build.bat
@@ -1,9 +1,6 @@
 @echo on
 
 pushd ACardEmulator
-call gradlew.bat task --all || goto :err
-call gradlew.bat sourceSets || goto :err
-call gradlew.bat check || goto :err
 call gradlew.bat compile || goto :err
 call gradlew.bat build || goto :err
 move *.apk %ARTIFACT% || goto :err
